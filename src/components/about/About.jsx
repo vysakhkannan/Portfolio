f1f5a92dc924aac3bfe5e8bb9bf52e58d139bb52
vysakhkannan <vysakhkannan811@gmail.com>
2022-02-28 16:34:29 +0530

    first commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './about.css'
+import ME from '../../assets/about_me.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+const About = () => {
+  return (
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>6 months Internship Experience</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>0</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>4+ Completed Projects</small>
+            </article>
+          </div>
+          <p>
+          A diligent and committed self-taugh Python Django Deveoper and BCA grad looking for an opportunity in the tech field. Who is committed to study web development. Mindset to fullfill the life with wonderful memories and knowledge.
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import HeaderSocials from './HeaderSocials'
+import ME from '../../assets/me.png'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Vysakh Kannan </h1>
+        <h5 className="text-light">Python Django Developer</h5>
+        <CTA/>
+        <HeaderSocials/>
+
+        <div className="me">
+          <img src={ME} alt='' />
+        </div>
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
